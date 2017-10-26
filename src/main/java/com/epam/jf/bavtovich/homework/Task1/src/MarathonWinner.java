4ff355176c98fe0eed23b1dc81041f0ad1feeb1b
iBavtovich <ignatbav@gmail.com>
2017-10-26 22:02:02 +0300

    Task 1

---
+++ src/main/java/com/epam/jf/bavtovich/homework/Task1/src/MarathonWinner.java
@@ -0,0 +1,27 @@
+public class MarathonWinner {
+
+    public static void main(String...args) {
+
+        String[] names = {"Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt", "Alex", "Emma",
+                "John", "James", "Jane", "Emily", "Daniel", "Neda", "Aaron", "Kate"};
+        int[] times = {341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299, 343, 317, 265};
+        int maxTimeIndex = 0;
+        int secondTimeIndex = 0;
+
+        for (int i = 1; i < names.length; i++) {
+
+            if (times[i] > times[maxTimeIndex]) {
+                secondTimeIndex = maxTimeIndex;
+                maxTimeIndex = i;
+            }
+            else {
+                if (times[i] > times[secondTimeIndex]) {
+                    secondTimeIndex = i;
+                }
+            }
+        }
+
+        System.out.println("Winner of marathon: " + names[maxTimeIndex]);
+        System.out.println("Second place: " + names[secondTimeIndex]);
+    }
+}

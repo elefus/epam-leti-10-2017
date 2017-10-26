b08fcf88b1ed22cd9d10b340cd84b17d88c69656
Steen8 <alex54le56@yandex.ru>
2017-10-26 23:01:34 +0300

    Добавлены решения первой и второй задач

---
+++ src/main/java/com/epam/jf/Kazakov/homework/Task2/main.java
@@ -0,0 +1,22 @@
+public class main {
+    public static void main(String...args) {
+        int maxStringLength = args[0].length();
+        int indexMaxStringLength = 0;
+        int minStringLength = args[0].length();
+        int indexMinStringLength = 0;
+
+        for (int i = 0; i < args.length; ++i) {
+            if(args[i].length() <  minStringLength) {
+                minStringLength = args[i].length();
+                indexMinStringLength = i;
+            }
+            if(args[i].length() > maxStringLength) {
+                maxStringLength = args[i].length();
+                indexMaxStringLength = i;
+            }
+        }
+
+        System.out.println("Минимальная строка среди представленных: " + args[indexMinStringLength]);
+        System.out.println("Максимальная строка среди представленных: " + args[indexMaxStringLength]);
+    }
+}

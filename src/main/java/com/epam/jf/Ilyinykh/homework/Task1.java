2ba4a23855cbbbf40a765af1f2f9984f46069c12
Ilya Ilyinykh <IlyasYOY@gmail.com>
2017-10-27 14:28:15 +0300

    Package changed.

--- src/main/java/com/epam/jf/Ilyinykh/homework/Task1.java
+++ src/main/java/com/epam/jf/Ilyinykh/homework/Task1.java
@@ -1,4 +1,4 @@
-package com.epam;
+package com.epam.jf.ilyinykh.homework;
 
 public class Task1 {
     public static void main(String[] args) {
--- src/main/java/com/epam/jf/Ilyinykh/homework/Task2.java
+++ src/main/java/com/epam/jf/Ilyinykh/homework/Task2.java
@@ -1,4 +1,4 @@
-package com.epam;
+package com.epam.jf.ilyinykh.homework;
 
 public class Task2 {
     public static void main(String[] args) {

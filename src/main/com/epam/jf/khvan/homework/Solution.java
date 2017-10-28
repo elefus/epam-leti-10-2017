c356b0e85163aa8567d8de4b6c8e0a9c4988c410
Dimitriy <kwanmitya@gmail.com>
2017-10-29 00:23:06 +0300

    with '#' will be ignored, and an empty message aborts the commit.
      On branch Khvan

--- src/main/com/epam/jf/khvan/homework/Solution.java
+++ src/main/com/epam/jf/khvan/homework/Solution.java
@@ -1,4 +1,9 @@
 
+/*
+Отсчет на гонках
+*/
+
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,36 +17,24 @@ public class Solution {
         String[] names = { "Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt", "Alex", "Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda", "Aaron", "Kate" };
         int[] times = { 341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299, 343, 317, 265 };
 
-        int [] timesSorted={ 341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299, 343, 317, 265 };
+        int first = 0;
+        int second = 0;
 
+        for(int i = 0; i <times.length; i++) {
 
-        for (int i = 0; i <timesSorted.length-1; i++) {
-            int min=timesSorted[i];
-
-            for (int j = i+1; j <timesSorted.length ; j++) {
-                if(min>timesSorted[j]){
-                  min=timesSorted[j];
-                  timesSorted[j]=timesSorted[i];
-                  timesSorted[i]=min;
-                }
-
+            if (   times[i] <=  times[first]    &&     first   != i ) {
+                second= first;
+                first = i;
             }
-        }
 
-        int first=0;
-        int second=0;
-
-        for (int i = 0; i < times.length; i++) {
-            if (times[i]==timesSorted[0])first=i;
-            if(times[i]==timesSorted[1])second=i;
-        }
-
-        System.out.println(names[first] +" "+timesSorted[0]);
-
-        System.out.println(names[second] +" "+timesSorted[1]);
+            if(   second!= i & times[second] < times[first] &   times[second]   >   times[i] ){
 
 
+                second=i;
+            } }
 
 
+        System.out.println("First " + names[first] + " " + times[first]);
+        System.out.println("Second " + names[second] + " " + times[second]);
     }
 }

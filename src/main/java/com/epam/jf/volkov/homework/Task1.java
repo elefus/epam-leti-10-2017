0bdee4fcced3642716651b5ea0d1f05454c2fbe1
thewalkingwind <tehwlaknigwnid@gmail.com>
2017-10-28 14:44:31 +0300

    Added message for no args run; Fixed names

--- src/main/java/com/epam/jf/volkov/homework/Task1.java
+++ src/main/java/com/epam/jf/volkov/homework/Task1.java
@@ -1,6 +1,6 @@
 package com.epam.jf.volkov.homework;
 
-public class RunnersTask {
+public class Task1 {
     public static void main(String...args) {
         String[] names = { "Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt", "Alex", "Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda", "Aaron", "Kate" };
         int[] times = { 341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299, 343, 317, 265 };
--- src/main/java/com/epam/jf/volkov/homework/Task2.java
+++ src/main/java/com/epam/jf/volkov/homework/Task2.java
@@ -1,6 +1,6 @@
 package com.epam.jf.volkov.homework;
 
-public class StringLengthTask {
+public class Task2 {
     public static void main (String...args) {
         if (args.length>0) {
             String max = args[0];
@@ -11,6 +11,6 @@ public class StringLengthTask {
             }
             System.out.println("\nString with a maximum length: "+max);
             System.out.println("String with a minimum length: "+min+"\n");
-        }
+        } else System.out.println("No args detected");
     }
 }

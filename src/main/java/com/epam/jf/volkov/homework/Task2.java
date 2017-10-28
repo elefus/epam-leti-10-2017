eaaac0b5c2a00e7ef505ce0dd3d8d297ef0b8ca4
thewalkingwind <tehwlaknigwnid@gmail.com>
2017-10-28 14:47:37 +0300

    Minor loop fix: changed 2 for-each

--- src/main/java/com/epam/jf/volkov/homework/Task2.java
+++ src/main/java/com/epam/jf/volkov/homework/Task2.java
@@ -5,9 +5,9 @@ public class Task2 {
         if (args.length>0) {
             String max = args[0];
             String min = args[0];
-            for (int i = 0; i<args.length; i++) {
-                if (args[i].length()>=max.length()) max = args[i];
-                if (args[i].length()<=min.length()) min = args[i];
+            for (String curr : args) {
+                if (curr.length()>=max.length()) max = curr;
+                if (curr.length()<=min.length()) min = curr;
             }
             System.out.println("\nString with a maximum length: "+max);
             System.out.println("String with a minimum length: "+min+"\n");

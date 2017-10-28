acf1b72fcff8c26f42b94dff6d65e3dbeb2d0504
RileanMayer <chingisesenbaev@gmail.com>
2017-10-29 01:02:32 +0300

    task_2 updated

--- src/main/java/com/epam/jf/yessenbayev/homework/Task_2.java
+++ src/main/java/com/epam/jf/yessenbayev/homework/Task_2.java
@@ -2,16 +2,26 @@ package main.java.com.epam.jf.yessenbayev.homework;
 
 public class Task_2 {
     public static void main(String[] args) {
-        int min = 0;
-        int max = 0;
 
-        for(int i = 0; i < args.length; i++){
-            if (args[i].length() >= args[max].length())
-                max = i;
-            else if(args[i].length() <= args[min].length())
-                min = i;
-        }
-        System.out.println("Min is " + args[min]);
-        System.out.println("Max is " + args[max]);
+
+        if (args.length > 0){
+
+            int max = 0;
+            int min = 0;
+
+            for(int i = 0; i < args.length; i++){
+                if (args[i].length() >= args[max].length()) max = i;
+                if (args[i].length() <= args[min].length()) min = i;
+
+            }
+
+            System.out.println("Max is " + args[max]);
+            System.out.println("Min is " + args[min]);
+
+        } else System.out.println("Error! Please input args");
+
+
+
     }
 }
+

06fbd8e1fabba752dd11ce6b57114c317a7d93f4
Dimitriy <kwanmitya@gmail.com>
2017-10-29 19:17:54 +0300

    Minor line fixes

--- src/main/com/epam/jf/khvan/homework/Solution.java
+++ src/main/com/epam/jf/khvan/homework/Solution.java
@@ -1,40 +1,28 @@
-
-/*
-Отсчет на гонках
-*/
-
-
+package com.epam.jf.khvan.homework.Solution.java
 
 import java.util.ArrayList;
 import java.util.List;
-
-/*
-???????? ??????
-*/
 
 public class Solution {
     public static void main(String[] args)  {
         String[] names = { "Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt", "Alex", "Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda", "Aaron", "Kate" };
         int[] times = { 341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299, 343, 317, 265 };
-
         int first = 0;
         int second = 0;
-
-        for(int i = 0; i <times.length; i++) {
-
-            if (   times[i] <=  times[first]    &&     first   != i ) {
+          for(int i = 0; i <times.length; i++) {
+              if (   times[i] <=  times[first]    &&     first   != i ) {
                 second= first;
                 first = i;
-            }
-
-            if(   second!= i & times[second] < times[first] &   times[second]   >   times[i] ){
-
-
-                second=i;
-            } }
-
-
+              }
+              if(   second!= i & times[second] < times[first] &   times[second]   >   times[i] ){
+                  second=i;
+              }}
         System.out.println("First " + names[first] + " " + times[first]);
         System.out.println("Second " + names[second] + " " + times[second]);
     }
 }
+
+
+
+
+
--- src/main/com/epam/jf/khvan/homework/StringsQuest.java
+++ src/main/com/epam/jf/khvan/homework/StringsQuest.java
@@ -1,32 +1,19 @@
-
-
-
-
-
-
+package com.epam.jf.khvan.homework.StringsQuest.java
 
 public class StringsQuest {
-
     public static void main(String [] m) {
         String shortest=m[0];
         String longest=m[0];
-
         for (int i = 0; i <m.length ; i++) {
             if(m[i].length()>=longest.length()){
                 longest=m[i];
             }
-
             if(m[i].length()<=shortest.length()){
                 shortest=m[i];
-            }
-        }
-
-
+            }}
         System.out.println(shortest);
-
         System.out.println(longest);
-
-
-
     }
 }
+
+

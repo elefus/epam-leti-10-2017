ae8dce09534b76ac97ed87d4981ed87f229ce199
Hitsugaya7 <sasuke_uchiha__@mail.ru>
2017-11-01 15:47:25 +0300

    Task1 is changed

--- src/main/java/com/epam/jf/ilyassov/homework/Task1.java
+++ src/main/java/com/epam/jf/ilyassov/homework/Task1.java
@@ -4,23 +4,21 @@ public class Task1 {
         String[] names = {"Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt", "Alex", "Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda", "Aaron", "Kate"};
         int[] times = {341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299, 343, 317, 265};
         int k = Integer.MAX_VALUE;
-        int index = 0;
-        int second = 0;
-        int index2 = 0;
-        int y = 0;
+        int min_index = 0;
+        int sec_index=0;
+        int second=times[0];
         for (int i = 0; i < times.length; ++i) {
-            index2++;
             if (k > times[i]) {
-                y = index2;
-                second = k;
                 k = times[i];
-                index = i;
-                index2 = 0;
+                min_index = i;
             }
-
+            else
+                {
+                    sec_index=i;
+                    second=times[i];
+                }
         }
-        System.out.println("First: " + names[index] + ", score: " + k);
-        System.out.println("Second: " + names[index - y] + " , score: " + second);
-
+        System.out.println("First: " + names[min_index] + ", score: " + k);
+        System.out.println("Second: " + names[sec_index] + " , score: " + second);
     }
 }

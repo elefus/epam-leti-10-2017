18829d5c6f2df2bdfa0fa55f7cc35e2d12ae41e7
Ilya Ilyinykh <IlyasYOY@gmail.com>
2017-11-02 19:53:02 +0300

    task5 finished.

--- src/main/java/com/epam/jf/ilyinykh/homework/Task5.java
+++ src/main/java/com/epam/jf/ilyinykh/homework/Task5.java
@@ -1,5 +1,7 @@
 package com.epam.jf.ilyinykh.homework;
 
+import java.util.Arrays;
+
 /**
  * На вход программе подается несколько строк (через параметры командной строки args).
  * Каждая строка представляет собой одно слово.
@@ -20,6 +22,68 @@ public class Task5 {
      * @param args анализируемые строки.
      */
     public static void main(String[] args) {
-        // TODO решение задачи
+        int counter = 0;
+        System.out.println("Arguments: " + Arrays.toString(args));
+        for (String word : args) {
+            if (isFits(word)) {
+                counter++;
+            }
+        }
+
+        System.out.println("Answer: " + counter);
+    }
+
+    /**
+     * @param word слово для анализа.
+     * @return подходит ли слово к требованиям задачи.
+     */
+    private static boolean isFits(final String word) {
+        char[] letters = word.toCharArray();
+        int numberOfConsonants = 0;
+        int numberOfVowels = 0;
+
+        for (char letter : letters) {
+            if (Character.isLetter(letter)) {
+                // Проверяем на гласность, т.к. их меньше.
+                if (isVowel(letter)) {
+                    numberOfVowels++;
+                } else {
+                    numberOfConsonants++;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        return numberOfConsonants == numberOfVowels;
+    }
+
+    /**
+     * @param letter букву для анализа.
+     * @return является ли буква гласной.
+     */
+    private static boolean isVowel(final char letter){
+        switch (letter) {
+            case 'a':
+            case 'A':
+
+            case 'e':
+            case 'E':
+
+            case 'i':
+            case 'I':
+
+            case 'o':
+            case 'O':
+
+            case 'u':
+            case 'U':
+
+            case 'y':
+            case 'Y':
+                return true;
+            default:
+                return false;
+        }
     }
 }

88e9203297913c6f588a0aee15d4e6a63550049a
Alex Burachenko <ss47305@gmail.com>
2017-11-03 00:02:25 +0300

    Исправление ошибок прошлого. Добавил файл в пакет ../homework. Переименовал класс с большой буквы. Теперь каждая переменная объявлена в отдельной строке. Вернул сбежавшие скобки. Сделал красивый вывод на русском.

---
+++ src/main/java/com/epam/jf/burachenko/homework/Task1.java
@@ -0,0 +1,37 @@
+package com.epam.jf.burachenko.homework;
+
+public class Task1 {
+	public static void main(String...args) {
+		String[] names = {"Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt", "Alex", "Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda", "Aaron", "Kate"};
+
+		int[] times = {341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299, 343, 317, 265};
+
+		int i_first = 0;
+		int i_second = 0;
+
+		int first;
+		int second;
+		if(times[0] < times[1]) {
+			first = times[0];
+			second = times[1];
+		}
+		else {
+			first = times[1];
+			second = times[0];
+		}
+
+		for (int i = 2; i < times.length; i++) {
+			if (times[i] < first) {
+				first = times[i];
+				i_first = i;
+			}
+			else
+			if (times[i] < second) {
+				second = times[i];
+				i_second = i;
+			}
+		}
+		System.out.println("Первым прибежал(а): " + names[i_first] + " за " + times[i_first]);
+		System.out.println("Вторым прибежал(а): " + names[i_second] + " за " + times[i_second]);
+	}
+}

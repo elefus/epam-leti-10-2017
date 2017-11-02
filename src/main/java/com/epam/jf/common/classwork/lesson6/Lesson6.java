8b650726ff94d8f4fd427c6e46e3143462288101
Andrewisnew <zakomornii1998@gmail.com>
2017-11-02 14:40:31 +0300

    Task 4 completed

--- src/main/java/com/epam/jf/common/classwork/lesson6/Lesson6.java
+++ src/main/java/com/epam/jf/common/classwork/lesson6/Lesson6.java
@@ -1,7 +1,7 @@
 package com.epam.jf.common.classwork.lesson6;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+//import org.jetbrains.annotations.NotNull;
+//import org.jetbrains.annotations.Nullable;
 
 import java.util.Scanner;
 
@@ -31,29 +31,29 @@ public class Lesson6 {
 
     private static void nullableExample() {
         Scanner scanner = new Scanner(System.in);
-        @NotNull String string = scanner.nextLine();
+       // @NotNull String string = scanner.nextLine();
 //        @NotNull String str1 = method(string);
     }
 
-    /**
-     * {@code int a = 30;}
-     * Value = {@value Lesson6#value}
-     * asdsad {@link Lesson6#getValues()}
-     * @param string
-     * @param a
-     * @param b
-     * @param j
-     * @return
-     * @throws IllegalAccessError
-     * @throws NullPointerException
-     * @see <a href="https://jcp.org/en/jsr/detail?id=335">JSR-335: Lambda Expressions for the Java Programming Language</a>
-     * @see Lesson6#isCellEditable(int)
-     *
-     */
-    public static @Nullable String method(@NotNull String string, int a, double b, int j) throws IllegalAccessError, NullPointerException {
-        System.out.println(string);
-        return "123";
-    }
+//    /**
+//     * {@code int a = 30;}
+//     * Value = {@value Lesson6#value}
+//     * asdsad {@link Lesson6#getValues()}
+//     * @param string
+//     * @param a
+//     * @param b
+//     * @param j
+//     * @return
+//     * @throws IllegalAccessError
+//     * @throws NullPointerException
+//     * @see <a href="https://jcp.org/en/jsr/detail?id=335">JSR-335: Lambda Expressions for the Java Programming Language</a>
+//     * @see Lesson6#isCellEditable(int)
+//     *
+//     */
+//    public static @Nullable String method(@NotNull String string, int a, double b, int j) throws IllegalAccessError, NullPointerException {
+//        System.out.println(string);
+//        return "123";
+//    }
 
     private static int[] getValues() {
         return new int[0];
--- src/main/java/com/epam/jf/zakomornyy/homework/Task5.java
+++ src/main/java/com/epam/jf/zakomornyy/homework/Task5.java
@@ -1,25 +1,59 @@
-package com.epam.jf.common.homework;
+package com.epam.jf.zakomornyy.homework;
 
-/**
- * На вход программе подается несколько строк (через параметры командной строки args).
- * Каждая строка представляет собой одно слово.
- * В результате выполнения в выходной поток должно быть выведено количество слов,
- * состоящих только из символов латинского алфавита и содержащих равное количество гласных и согласных букв.
- *
- * Количество различных вхождений одной буквы в слове учитывается.
- *
- * Входные данные:
- * args = ["Язык", "программирования", "Java", "is", "widespread"]
- *
- * Выходные данные:
- * 2
- */
+import java.util.LinkedList;
+
+
 public class Task5 {
 
     /**
      * @param args анализируемые строки.
      */
     public static void main(String[] args) {
-        // TODO решение задачи
+        LinkedList<String> onlyLatinLetters = getLatinStrings(args);
+        System.out.println(numOfStrWithEqualNumOfVowAndConsLetters(onlyLatinLetters));
+    }
+
+    public static LinkedList<String> getLatinStrings(String[] args){
+        LinkedList<String> onlyLatinStrings = new LinkedList<>();
+        for (String str: args) {
+            if(consistOnlyLatinLetters(str)) {
+                onlyLatinStrings.add(str);
+            }
+        }
+        return onlyLatinStrings;
+    }
+
+    public static boolean consistOnlyLatinLetters(String str) {
+        for (char letter: str.toCharArray()) {
+            if((letter < 'A' || letter > 'Z') && (letter < 'a' || letter > 'z')) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int numOfStrWithEqualNumOfVowAndConsLetters(LinkedList<String> onlyLatinLetters) {
+        char[] vowelLetters = {'a', 'e', 'i', 'o', 'u', 'y'};
+        int count = 0;
+        for (String str: onlyLatinLetters) {
+            int charCounter = 0;
+            String strInLowCase = str.toLowerCase();
+            /* можно было не использовать метку, тогда пришлось бы в блоке if для проверки на гласную писать
+             charCounter += 2; break; что снизило бы читаемость, стоило ли он того?
+            */
+            outer: for (char letter: strInLowCase.toCharArray()) {
+                for (char vowelLetter : vowelLetters) {
+                    if (vowelLetter == letter) {
+                        charCounter++;
+                        continue outer;
+                    }
+                }
+                charCounter--;
+            }
+            if(charCounter == 0) {
+                count++;
+            }
+        }
+        return count;
     }
 }

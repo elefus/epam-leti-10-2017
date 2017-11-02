a7100d9f048517bc009b8d31500e377f4808e37b
Andrewisnew <zakomornii1998@gmail.com>
2017-11-02 14:44:59 +0300

    Task 5 completed

--- src/main/java/com/epam/jf/common/classwork/lesson6/Lesson6.java
+++ src/main/java/com/epam/jf/common/classwork/lesson6/Lesson6.java
@@ -1,7 +1,7 @@
 package com.epam.jf.common.classwork.lesson6;
 
-//import org.jetbrains.annotations.NotNull;
-//import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Scanner;
 
@@ -18,11 +18,11 @@ public class Lesson6 {
 
 
     public static void main(String[] args) {
-//        nullableExample();
-//        for (int i : getValues()) {
-//            System.out.println(i);
-//        }
-//        Optional<Integer> integer = Optional.of(1);
+        nullableExample();
+        for (int i : getValues()) {
+            System.out.println(i);
+        }
+        Optional<Integer> integer = Optional.of(1);
 
         Lesson6 obj = null;
         obj.getValues();
@@ -31,29 +31,29 @@ public class Lesson6 {
 
     private static void nullableExample() {
         Scanner scanner = new Scanner(System.in);
-       // @NotNull String string = scanner.nextLine();
-//        @NotNull String str1 = method(string);
+        @NotNull String string = scanner.nextLine();
+        @NotNull String str1 = method(string);
     }
 
-//    /**
-//     * {@code int a = 30;}
-//     * Value = {@value Lesson6#value}
-//     * asdsad {@link Lesson6#getValues()}
-//     * @param string
-//     * @param a
-//     * @param b
-//     * @param j
-//     * @return
-//     * @throws IllegalAccessError
-//     * @throws NullPointerException
-//     * @see <a href="https://jcp.org/en/jsr/detail?id=335">JSR-335: Lambda Expressions for the Java Programming Language</a>
-//     * @see Lesson6#isCellEditable(int)
-//     *
-//     */
-//    public static @Nullable String method(@NotNull String string, int a, double b, int j) throws IllegalAccessError, NullPointerException {
-//        System.out.println(string);
-//        return "123";
-//    }
+    /**
+     * {@code int a = 30;}
+     * Value = {@value Lesson6#value}
+     * asdsad {@link Lesson6#getValues()}
+     * @param string
+     * @param a
+     * @param b
+     * @param j
+     * @return
+     * @throws IllegalAccessError
+     * @throws NullPointerException
+     * @see <a href="https://jcp.org/en/jsr/detail?id=335">JSR-335: Lambda Expressions for the Java Programming Language</a>
+     * @see Lesson6#isCellEditable(int)
+     *
+     */
+    public static @Nullable String method(@NotNull String string, int a, double b, int j) throws IllegalAccessError, NullPointerException {
+        System.out.println(string);
+        return "123";
+    }
 
     private static int[] getValues() {
         return new int[0];

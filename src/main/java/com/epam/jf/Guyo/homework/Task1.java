a7cb8e447583f8f0025588a79da35caeccf03db1
ampiq <davidqa2011@yandex.ru>
2017-11-04 22:15:13 +0300

    Изменения в Task1, алгоритм работает на всех последовательностях.

--- src/main/java/com/epam/jf/Guyo/homework/Task1.java
+++ src/main/java/com/epam/jf/Guyo/homework/Task1.java
@@ -6,10 +6,16 @@ class Task1{
 
         String[] names = { "Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt", "Alex", "Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda", "Aaron", "Kate" };
         int[] times = {341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299, 343, 317, 265};
-        int indexFirst = times.length-2;
-        int indexSecond = times.length-1;
+        int indexFirst = 0;
+        int indexSecond = 1;
+        int temp;
 
-        for (int i = 0; i < times.length ; i++) {
+        if (times[indexSecond] < times[indexFirst]){
+            temp = indexFirst;
+            indexFirst = indexSecond;
+            indexSecond = temp;
+        }
+        for (int i = 2; i < times.length ; i++) {
             if(times[i] < times[indexFirst]) {
                 indexSecond = indexFirst;
                 indexFirst = i;

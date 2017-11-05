7a7159231e9ca6f9f41a151d9989301d0d186521
kruglikalexey <kruglikalexey@ya.ru>
2017-11-05 14:28:14 +0300

    try to fix merge conflict

---
+++ src/main/java/com/epam/jf/kruglik/homework/Task2.java
@@ -0,0 +1,22 @@
+package com.epam.jf.kruglik.homework;
+
+public class Task2 {
+
+    public static void main(String...args) {
+        String min = args[0];
+        String max = args[0];
+
+       for (String s :args){
+            if (s.length() >= max.length()) {
+                max = s;
+            }
+
+            if (s.length() <= min.length()) {
+                min = s;
+            }
+        }
+
+        System.out.println("Max string: " + max);
+        System.out.println("Min string: " + min);
+    }
+}

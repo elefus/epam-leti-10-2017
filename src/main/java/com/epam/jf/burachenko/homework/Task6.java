76163b603c9ab72d869263a6ad49ccc4daf08043
Alex Burachenko <ss47305@gmail.com>
2017-11-07 15:39:33 +0300

    В Task 6 добавлено небольшое упрощение.

--- src/main/java/com/epam/jf/burachenko/homework/Task6.java
+++ src/main/java/com/epam/jf/burachenko/homework/Task6.java
@@ -16,12 +16,7 @@ public class Task6 {
     private static boolean isFirstInAlphabet(String s1, String s2) {
         for(int i = 0; i < s1.length(); i++) {
             if(s1.charAt(i) != s2.charAt(i)) {
-                if(s1.charAt(i) < s2.charAt(i)) {
-                    return true;
-                }
-                else {
-                    return false;
-                }
+                return s1.charAt(i) < s2.charAt(i);
             }
         }
         return false;

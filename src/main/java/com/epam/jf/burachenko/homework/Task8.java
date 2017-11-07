ca179a9cda04cf18d2d125407014982c2d1393d1
Alex Burachenko <ss47305@gmail.com>
2017-11-08 00:14:54 +0300

    Task 8 done.

--- src/main/java/com/epam/jf/burachenko/homework/Task8.java
+++ src/main/java/com/epam/jf/burachenko/homework/Task8.java
@@ -13,10 +13,33 @@ package com.epam.jf.burachenko.homework;
  */
 public class Task8 {
 
+    private static boolean isPalindrome(String word) {
+        int length = word.length();
+        int border = (length%2 == 0) ? length/2 : length/2+1;
+        for(int i = 0; i < border; i++) {
+            if(!(word.charAt(i) == word.charAt(length-i-1) && word.charAt(i) >= '0' && word.charAt(i) <= '9')) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     /**
      * @param args анализируемые строки.
      */
     public static void main(String[] args) {
+        int countOfPalindromes = 0;
+        String result = null;
+        for (String word: args) {
+            if(isPalindrome(word)) {
+                countOfPalindromes++;
+                result = word;
+                if(countOfPalindromes == 2) {
+                    break;
+                }
+            }
+        }
 
+        System.out.println(result);
     }
 }

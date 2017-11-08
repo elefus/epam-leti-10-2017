5a6144ef25bd8921d821273e2c6de6bb7cbaeb19
Egakupi <dmisavchenkv@gmail.com>
2017-11-08 12:58:11 +0300

    Initial Task7

---
+++ src/main/java/com/epam/jf/savchenkov/homework/task7/AbstractIntArrayList.java
@@ -0,0 +1,36 @@
+package com.epam.jf.savchenkov.homework.task7;
+
+public abstract class AbstractIntArrayList {
+
+    protected int[] values;
+    protected int size;
+    
+
+    public abstract boolean add(int value);
+
+    public abstract boolean add(int value, int index);
+
+    public abstract int get(int index);
+
+    public abstract boolean contains(int value);
+
+    public abstract boolean containsAll(AbstractIntArrayList list);
+
+    public abstract int remove(int index);
+
+    public abstract int set(int value, int index);
+
+    public abstract boolean addAll(AbstractIntArrayList list);
+
+    public abstract void clear();
+
+    public abstract int size();
+
+    public abstract boolean isEmpty();
+
+    public abstract int indexOf(int value);
+
+    public abstract int lastIndexOf(int value);
+
+    public abstract AbstractIntArrayList subList(int fromInclusive, int toInclusive);
+}
---
+++ src/main/java/com/epam/jf/savchenkov/homework/task7/IntArrayList.java
@@ -0,0 +1,74 @@
+package com.epam.jf.savchenkov.homework.task7;
+
+public class IntArrayList extends AbstractIntArrayList {
+
+    @Override
+    public boolean add(int value) {
+        return false;
+    }
+
+    @Override
+    public boolean add(int value, int index) {
+        return false;
+    }
+
+    @Override
+    public int get(int index) {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(int value) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(AbstractIntArrayList list) {
+        return false;
+    }
+
+    @Override
+    public int remove(int index) {
+        return 0;
+    }
+
+    @Override
+    public int set(int value, int index) {
+        return 0;
+    }
+
+    @Override
+    public boolean addAll(AbstractIntArrayList list) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int indexOf(int value) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(int value) {
+        return 0;
+    }
+
+    @Override
+    public AbstractIntArrayList subList(int fromInclusive, int toInclusive) {
+        return null;
+    }
+}

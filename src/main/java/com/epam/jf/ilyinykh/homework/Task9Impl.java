7b94499aad158a37f8dc7c507915153ad71f3167
Ilya Ilyinykh <IlyasYOY@gmail.com>
2017-11-08 18:54:26 +0300

    Task9 done and tests added

--- src/main/java/com/epam/jf/ilyinykh/homework/Task9Impl.java
+++ src/main/java/com/epam/jf/ilyinykh/homework/Task9Impl.java
@@ -1,11 +1,20 @@
-package com.epam.jf.ilyinykh.homework.task9;
+package com.epam.jf.ilyinykh.homework;
 
-public abstract class Task9 {
+import com.epam.jf.common.homework.Task9;
 
+public class Task9Impl extends Task9 {
     /**
      * С помощью битовых и логических операций определяет, является ли указанное значение степенью двойки.
+     *
      * @param value анализируемое не отрицательное число.
      * @return true - число является степенью двойки, false в противном случае.
      */
-    public abstract boolean isPowerOfTwo(long value);
+    @Override
+    public boolean isPowerOfTwo(long value) {
+        return value != 0 && (value & (value - 1)) == 0;
+    }
+
+    public static void main(String...args) {
+        System.out.println(new Task9Impl().isPowerOfTwo(0));
+    }
 }

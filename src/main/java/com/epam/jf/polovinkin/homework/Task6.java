10a8cca8a2314ffec0c43b5a4a0fe182b97b329b
Awethon <gorodgorod63@gmail.com>
2017-11-09 19:33:16 +0300

    task 6

---
+++ src/main/java/com/epam/jf/polovinkin/homework/Task6.java
@@ -0,0 +1,19 @@
+package com.epam.jf.polovinkin.homework;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * На вход программе подается несколько строк (через параметры командной строки args).
+ * Требуется упорядочить и вывести строки в порядке возрастания значений их длины.
+ * В случае, если длины строк совпадают - упорядочить их в лексикографическом порядке.
+ */
+public class Task6 {
+
+    /**
+     * @param args анализируемые строки.
+     */
+    public static void main(String[] args) {
+        Arrays.stream(args).sorted().sorted(Comparator.comparingInt(String::length)).forEach(System.out::println);
+    }
+}

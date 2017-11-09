fb55336a12bf35b64874e3dd4d14b19c3826b360
iBavtovich <ignatbav@gmail.com>
2017-11-09 18:08:34 +0300

    Task 9 completed

---
+++ src/main/java/com/epam/jf/bavtovich/homework/Task9.java
@@ -0,0 +1,13 @@
+package com.epam.jf.bavtovich.homework;
+
+public class Task9 extends com.epam.jf.common.homework.Task9 {
+
+    @Override
+    public boolean isPowerOfTwo(long value) {
+        if (value > 0) {
+            return (value & 1L) != 1L && (Long.bitCount(value) == 1);
+        } else {
+            return false;
+        }
+    }
+}

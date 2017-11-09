b50e588bbefd341f4402f245d16dc343e3798648
Awethon <gorodgorod63@gmail.com>
2017-11-10 00:05:43 +0300

    task 9

---
+++ src/main/java/com/epam/jf/polovinkin/homework/Task9.java
@@ -0,0 +1,13 @@
+package com.epam.jf.polovinkin.homework;
+
+public abstract class Task9 {
+
+    /**
+     * С помощью битовых и логических операций определяет, является ли указанное значение степенью двойки.
+     * @param value анализируемое не отрицательное число.
+     * @return true - число является степенью двойки, false в противном случае.
+     */
+    public boolean isPowerOfTwo(long value) {
+        return (value != 0) && ((value & (value - 1)) == 0);
+    }
+}

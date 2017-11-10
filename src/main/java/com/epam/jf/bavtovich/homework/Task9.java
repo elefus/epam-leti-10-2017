8a55adfd361f1834198707b2ae23f85a6af69871
iBavtovich <ignatbav@gmail.com>
2017-11-10 07:51:04 +0300

    Task 9 some changes

--- src/main/java/com/epam/jf/bavtovich/homework/Task9.java
+++ src/main/java/com/epam/jf/bavtovich/homework/Task9.java
@@ -5,7 +5,7 @@ public class Task9 extends com.epam.jf.common.homework.Task9 {
     @Override
     public boolean isPowerOfTwo(long value) {
         if (value > 0) {
-            return (value & 1L) != 1L && (Long.bitCount(value) == 1);
+            return (value % 2 == 0) && (Long.bitCount(value) == 1);
         } else {
             return false;
         }

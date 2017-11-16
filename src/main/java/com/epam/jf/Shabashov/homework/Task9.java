a0cf3f49f8b61223f5365114c766e798712d5bed
NikitaShabashov <niksh81@gmail.com>
2017-11-16 06:47:05 +0300

    fixed at input:1

--- src/main/java/com/epam/jf/Shabashov/homework/Task9.java
+++ src/main/java/com/epam/jf/Shabashov/homework/Task9.java
@@ -4,7 +4,6 @@ package com.epam.jf.Shabashov.homework;
 public class Task9 extends com.epam.jf.common.homework.Task9{
 
     public boolean isPowerOfTwo(long value) {
-        if ((value & 1L) != 0) return false;
         if (Long.bitCount(value)==1) return true;
         return false;
     }

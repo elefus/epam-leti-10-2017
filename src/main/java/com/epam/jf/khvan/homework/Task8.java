72ef3d89d8a98e53185b08e9f44bff3682d0fdca
Dimitriy <kwanmitya@gmail.com>
2017-11-16 19:48:35 +0300

    Task8 solution fixed

--- src/main/java/com/epam/jf/khvan/homework/Task8.java
+++ src/main/java/com/epam/jf/khvan/homework/Task8.java
@@ -18,26 +18,25 @@ public class Task8 {
      * @param args анализируемые строки.
      */
     public static void main(String[] args) {
-        int firstPalindrom = -1;
-        int secondPalindroim = -2;
+        int firstPalindrom = -123;
+
         int m = 0;
         boolean flag;
         for (String word: args) {
             flag = true;
             char [] charArray = word.toCharArray();
             for ( int i = 0; i < charArray.length; i++){
-                if ( charArray[i] != charArray[charArray.length - 1 - i] || !(Character.isDigit(charArray[i])) ){
+                if ( charArray[i] != charArray[charArray.length - 1 - i] || !( Character.isDigit(charArray[i])) ){
                     flag = false;
                 }
             }
             if ( flag){
                 m++;
                 if ( firstPalindrom < 0) { firstPalindrom = Integer.parseInt(word);}
-                if ( secondPalindroim < 0 & m == 2) { secondPalindroim = Integer.parseInt(word);}
+                if (  m == 2) { firstPalindrom = Integer.parseInt(word);}
             }
         }
-        System.out.println(firstPalindrom);
-        System.out.println(secondPalindroim);
+        System.out.println( firstPalindrom);
     }
 }
 

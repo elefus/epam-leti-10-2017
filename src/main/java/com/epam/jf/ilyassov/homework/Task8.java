0291b3afc739ac5cbe6c0b5ebf9faf8c80f809a7
Hitsugaya7 <sasuke_uchiha__@mail.ru>
2017-11-17 03:01:21 +0300

    Task8 is fixed

--- src/main/java/com/epam/jf/ilyassov/homework/Task8.java
+++ src/main/java/com/epam/jf/ilyassov/homework/Task8.java
@@ -6,27 +6,37 @@ public class Task8 {
         if(args.length==0){
             return;
         }
+        int count=0;
         String palidrom="";
-        int second=0;
-        for (int i = 0; i < args.length; i++) {
-            if(methodPalidrom(args[i])&&second<2)
-            {
-                palidrom=args[i];
-                second++;
+        for (String arg : args) {
+            if(Number(arg)&&Palidrom(arg)&&count<2){
+                palidrom=arg;
+                count++;
             }
+
         }
-        System.out.println(palidrom);
+        System.out.println("Число-палиндром "+palidrom);
     }
 
-    public static boolean methodPalidrom(String arr)
+    public static boolean Number(String arr)
     {
-        for(int i=0,j=arr.length()-1;i<arr.length()/2;++i,--j){
-            if(arr.charAt(i)!=arr.charAt(j))
+        for(int i=0;i<arr.length();++i){
+            if(arr.charAt(i)!='0'&&arr.charAt(i)!='1'&&arr.charAt(i)!='2'&&arr.charAt(i)!='3'&&arr.charAt(i)!='4' &&
+                    arr.charAt(i)!='5'&&arr.charAt(i)!='6'&&arr.charAt(i)!='7'&&arr.charAt(i)!='8'&&arr.charAt(i)!='9')
             {
                 return false;
             }
         }
         return true;
 
+    }
+
+    public static boolean Palidrom(String arg){
+        for(int i=0,j=arg.length()-1;i<arg.length()/2;++i,--j){
+            if(arg.charAt(i)!=arg.charAt(j)){
+                return false;
+            }
+        }
+        return true;
     }
 }

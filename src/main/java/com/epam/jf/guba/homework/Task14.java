72c8d386f628c58e2c0b1741573d98900901166d
Guba Dmitriy <gudmian@gmai.com>
2017-11-17 09:13:30 +0300

    Fix matrix creation

--- src/main/java/com/epam/jf/guba/homework/Task14.java
+++ src/main/java/com/epam/jf/guba/homework/Task14.java
@@ -4,26 +4,29 @@ public class Task14 extends com.epam.jf.common.homework.Task14 {
 
     @Override
     public int[][] sortMatrixByValuesInColumn(int[][] matrix, int indexColumn) {
-        if(indexColumn < 0 || indexColumn >= matrix.length){
+        if (indexColumn < 0 || indexColumn >= matrix.length) {
             throw new IllegalArgumentException("indexColumn is out of bound");
+        }
+        int[][] newMatrix = new int[matrix.length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                newMatrix[i][j] = matrix[i][j];
+            }
         }
         int[] temp;
         int j;
-        for(int i = 0; i < matrix.length - 1; i++){
-            if (matrix[i][indexColumn] > matrix[i + 1][indexColumn]) {
-                temp = matrix[i + 1];
-                matrix[i + 1] = matrix[i];
+        for (int i = 0; i < newMatrix.length - 1; i++) {
+            if (newMatrix[i][indexColumn] > newMatrix[i + 1][indexColumn]) {
+                temp = newMatrix[i + 1];
+                newMatrix[i + 1] = newMatrix[i];
                 j = i;
-                while (j > 0 && temp[indexColumn] < matrix[j - 1][indexColumn]) {
-                    matrix[j] = matrix[j - 1];
+                while (j > 0 && temp[indexColumn] < newMatrix[j - 1][indexColumn]) {
+                    newMatrix[j] = newMatrix[j - 1];
                     j--;
                 }
-                matrix[j] = temp;
+                newMatrix[j] = temp;
             }
         }
-
-        System.out.println(matrix);
-
-        return matrix;
+        return newMatrix;
     }
 }

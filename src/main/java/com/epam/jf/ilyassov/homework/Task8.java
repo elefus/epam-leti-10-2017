85342fa2b41cf84acb69b40c0d500d3bfca1c427
Hitsugaya7 <sasuke_uchiha__@mail.ru>
2017-11-17 03:15:00 +0300

    Task8 is fixed, use regex

--- src/main/java/com/epam/jf/ilyassov/homework/Task8.java
+++ src/main/java/com/epam/jf/ilyassov/homework/Task8.java
@@ -21,9 +21,7 @@ public class Task8 {
     public static boolean Number(String arr)
     {
         for(int i=0;i<arr.length();++i){
-            if(arr.charAt(i)!='0'&&arr.charAt(i)!='1'&&arr.charAt(i)!='2'&&arr.charAt(i)!='3'&&arr.charAt(i)!='4' &&
-                    arr.charAt(i)!='5'&&arr.charAt(i)!='6'&&arr.charAt(i)!='7'&&arr.charAt(i)!='8'&&arr.charAt(i)!='9')
-            {
+            if(!arr.matches("\\d+")) {
                 return false;
             }
         }

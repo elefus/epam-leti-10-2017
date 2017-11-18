e53067a0c6b670b01491d6fe7f51c923495e5385
Alex Burachenko <ss47305@gmail.com>
2017-11-18 23:27:07 +0300

    Сделал Task 11 по-другому

--- src/main/java/com/epam/jf/burachenko/homework/Task11.java
+++ src/main/java/com/epam/jf/burachenko/homework/Task11.java
@@ -1,17 +1,8 @@
 package com.epam.jf.burachenko.homework;
 
 class Task11 extends com.epam.jf.common.homework.Task11 {
-    private int degree = 1;
-
     @Override
     public int reverse(int value) {
-        if(value > 9) {
-            int temp = reverse(value / 10);
-            degree *= 10;
-            return value%10*degree + temp;
-        }
-        else {
-            return value%10;
-        }
+        return (value > 9) ? value%10*(int)Math.pow(10, Math.floor(Math.log10(value))) + reverse(value/10) : value%10;
     }
 }

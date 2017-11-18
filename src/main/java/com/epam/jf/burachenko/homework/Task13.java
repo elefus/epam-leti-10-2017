271156772e8ebf8b6633245f4ba684bb9fc039e2
Alex Burachenko <ss47305@gmail.com>
2017-11-18 23:43:07 +0300

    Переделал Task 13

--- src/main/java/com/epam/jf/burachenko/homework/Task13.java
+++ src/main/java/com/epam/jf/burachenko/homework/Task13.java
@@ -2,26 +2,18 @@ package com.epam.jf.burachenko.homework;
 
 public class Task13 extends com.epam.jf.common.homework.Task13 {
 
-    private int[] incrementEveryElementFor(int[] array, int increment) {
-        int[] incrementedArray = new int[array.length];
-        for(int i = 0; i < incrementedArray.length; i++) {
-            incrementedArray[i] = array[i] + increment;
-        }
-        return incrementedArray;
-    }
-
     @Override
     public int[][] createSquareMatrix(int dimension) {
         if(dimension == 0) {
             return null;
         }
 
+        int count = 1;
         int Matrix[][] = new int[dimension][dimension];
         for (int i = 0; i < dimension; i++) {
-            Matrix[0][i] = i+1;
-        }
-        for (int i = 1; i < dimension; i++) {
-            Matrix[i] = incrementEveryElementFor(Matrix[i-1], dimension);
+            for (int j = 0; j < dimension; j++) {
+                Matrix[i][j] = count++;
+            }
         }
         return Matrix;
     }

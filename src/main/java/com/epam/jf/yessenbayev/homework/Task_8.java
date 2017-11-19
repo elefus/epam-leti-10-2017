e942d322e7104dadd82c45cafccbf378d0921480
RileanMayer <chingisesenbaev@gmail.com>
2017-11-19 20:20:17 +0300

    added soluiton for task 8

---
+++ src/main/java/com/epam/jf/yessenbayev/homework/Task_8.java
@@ -0,0 +1,48 @@
+package main.java.com.epam.jf.yessenbayev.homework;
+
+public class Task_8 {
+    /**
+     *
+     * @param args анализируемые строки
+     */
+    public static void main(String[] args) {
+        if (args == null || args.length < 1) {
+            System.out.println("Недостаточно аргументов");
+            return;
+        }
+
+        int answer = -1;
+        int count = 0;
+        for (String word : args) {
+            if (isDigit(word)) {
+                if (isPalindrome(word)) {
+                    answer = count;
+
+                    if (count == 2) {
+                        break;
+                    }
+                }
+            }
+            count++;
+        }
+
+        if (answer == 0) {
+            System.out.println("Числовых палнидромов нет");
+        } else {
+            System.out.println(args[answer]);
+        }
+    }
+
+    private static boolean isPalindrome(String word) {
+        for (int i = 0; i < word.length() / 2 + 1; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isDigit(String word) {
+        return word.matches("[0-9]*");
+    }
+}

391afd793f8efa607b690a0d9cac807e33a65980
Hitsugaya7 <sasuke_uchiha__@mail.ru>
2017-11-22 05:21:30 +0300

    Task13 completed

--- src/main/test/com/epam/jf/ilyassov/homework/Task13ImplTest.java
+++ src/main/test/com/epam/jf/ilyassov/homework/Task13ImplTest.java
@@ -3,11 +3,21 @@ package com.epam.jf.ilyassov.homework;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class Task13ImplTest {
 
     @Test
-    void testMaxLongValue() {
-        
+    void createSquareMatrixTest() {
+        Task13Impl task13 = new Task13Impl();
+        int[][] ints1=task13.createSquareMatrix(1);
+        int[][] ints3=task13.createSquareMatrix(3);
+        int[][] temp1={{1}};
+        int[][] temp3={{1,2,3},{4,5,6},{7,8,9}};
+
+        assertArrayEquals(ints1,temp1);
+        assertArrayEquals(ints3,temp3);
+
 
     }
 }

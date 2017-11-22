1fe615abe215b566ae459df136acad61f75957df
Egakupi <dmisavchenkv@gmail.com>
2017-11-22 12:09:20 +0300

    Completed Task10 with tests

--- src/main/java/com/epam/jf/savchenkov/homework/Task10.java
+++ src/main/java/com/epam/jf/savchenkov/homework/Task10.java
@@ -1,4 +1,8 @@
 package com.epam.jf.savchenkov.homework;
 
-public class Task10 {
+public class Task10 extends com.epam.jf.common.homework.Task10 {
+    @Override
+    public long getSumDigits(long value) {
+        return (value > 0) ? (value % 10 + getSumDigits(value / 10)) : 0;
+    }
 }

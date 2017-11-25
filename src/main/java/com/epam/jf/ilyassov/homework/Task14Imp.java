a32f7d2f6620d3e44f4238dd5255769df25ca913
Hitsugaya7 <sasuke_uchiha__@mail.ru>
2017-11-25 21:53:03 +0300

    not completed

---
+++ src/main/java/com/epam/jf/ilyassov/homework/Task14Imp.java
@@ -0,0 +1,17 @@
+package com.epam.jf.ilyassov.homework;
+
+import com.epam.jf.common.homework.Task14;
+
+import java.util.Comparator;
+
+public class Task14Imp extends Task14{
+    @Override
+    public int[][] sortMatrixByValuesInColumn(int[][] matrix, int indexColumn) {
+        for (int i = 0; i < matrix.length; i++) {
+
+        }
+        return new int[0][];
+    }
+
+    Comparator()
+}

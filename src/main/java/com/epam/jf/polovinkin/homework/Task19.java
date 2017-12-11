c9a43a5c71acdaf1bbdbe9645e73d0e87debbb3f
Awethon <gorodgorod63@gmail.com>
2017-12-11 21:24:39 +0300

    task19 all test passed

--- src/main/java/com/epam/jf/polovinkin/homework/Task19.java
+++ src/main/java/com/epam/jf/polovinkin/homework/Task19.java
@@ -6,8 +6,8 @@ public class Task19 extends com.epam.jf.common.homework.Task19 {
         int counter = 0;
         int[][] temp = new int[source[0].length][source.length];
         for (int i = 0; i < source[0].length; i++) {
-            for (int j = source.length; j >= 0 ; j--) {
-                temp[i][j] = source[counter / source[0].length][counter % source[0].length];
+            for (int j = source.length - 1; j >= 0 ; j--) {
+                temp[j][i] = source[counter / source[0].length][counter % source[0].length];
                 counter++;
             }
         }

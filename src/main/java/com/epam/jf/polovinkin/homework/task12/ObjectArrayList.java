783f8425558db2950667e5c92cfc5c6ed340d5e2
Awethon <gorodgorod63@gmail.com>
2017-12-11 18:09:10 +0300

    impl

---
+++ src/main/java/com/epam/jf/polovinkin/homework/task12/ObjectArrayList.java
@@ -0,0 +1,180 @@
+package com.epam.jf.polovinkin.homework.task12;
+
+import com.epam.jf.common.homework.task12.AbstractObjectArrayList;
+import java.util.Objects;
+
+public class ObjectArrayList extends AbstractObjectArrayList {
+    protected Object[] values;
+    protected int pointer;
+    public ObjectArrayList() {
+        values = new Object[10];
+    }
+
+    public ObjectArrayList(int capacity) {
+        if (capacity < 0) throw new IllegalArgumentException("Capacity is below zero");
+        values = new Object[capacity];
+    }
+
+    public ObjectArrayList(ObjectArrayList list) {
+        values = list.values.clone();
+        pointer = list.pointer;
+    }
+
+    public ObjectArrayList(Object[] list) {
+        values = list.clone();
+        pointer = list.length;
+    }
+
+    @Override
+    public boolean add(Object value) {
+        if (pointer >= values.length) {
+            increaseCapacity();
+        }
+        values[++pointer] = value;
+        return true;
+
+    }
+
+    @Override
+    public boolean add(Object value, int index) {
+        if (index < 0 || index > pointer) {
+            throw new IllegalArgumentException("index is out of range");
+        }
+        if (pointer >= values.length) {
+            increaseCapacity();
+        }
+        System.arraycopy(values, index, values, index + 1, pointer - index );
+        values[index] = value;
+        pointer++;
+        return true;
+    }
+
+    @Override
+    public Object get(int index) {
+        if (index < 0 || index >= pointer) {
+            throw new IllegalArgumentException("index is out of range");
+        }
+        return values[index];
+    }
+
+    @Override
+    public boolean contains(Object value) {
+        for (int i = 0; i < pointer; i++) {
+            if (Objects.equals(values[i], value)) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(AbstractObjectArrayList list) {
+        for (int i = 0; i < list.size(); i++) {
+            if (!list.contains(list.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public Object remove(int index) {
+        if (index < 0 || index >= pointer) {
+            throw new IllegalArgumentException("index is out of range");
+        }
+        Object value = values[index];
+        System.arraycopy(values, index + 1, values, index, pointer - index - 1);
+        pointer--;
+        return value;
+    }
+
+    @Override
+    public Object remove(Object value) {
+        for (int i = 0; i < pointer; i++) {
+            if (Objects.equals(values[i], value)) {
+                return remove(i);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean removeAll(AbstractObjectArrayList list) {
+        return false;
+    }
+
+    @Override
+    public Object set(Object value, int index) {
+        if (index < 0 || index >= pointer) {
+            throw new IllegalArgumentException("index is out of range");
+        }
+        Object temp = values[index];
+        values[index] = value;
+        return temp;
+
+    }
+
+    @Override
+    public boolean addAll(AbstractObjectArrayList list) {
+        int freeSpace = values.length - pointer;
+        int spaceRequired = list.size();
+        if (freeSpace < spaceRequired) {
+            Object[] temp = new Object[values.length + spaceRequired + 1];
+            System.arraycopy(values, 0, temp, 0, pointer);
+            values = temp;
+        }
+        for (int i = 0; i < list.size(); i++) {
+            values[pointer + i] = list.get(i);
+        }
+        pointer += spaceRequired;
+        return true;
+
+    }
+
+    @Override
+    public void clear() {
+        pointer = 0;
+    }
+
+    @Override
+    public int size() {
+        return pointer;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return pointer == 0;
+    }
+
+    @Override
+    public int indexOf(Object value) {
+        for (int i = 0; i < pointer; i++) {
+            if (Objects.equals(values[i], value)) return i;
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object value) {
+        for (int i = pointer - 1; i >= 0; i--) {
+            if (Objects.equals(values[i], value)) return i;
+        }
+        return -1;
+
+    }
+
+    @Override
+    public AbstractObjectArrayList subList(int fromInclusive, int toInclusive) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public void trimToSize() {
+        //TODO
+    }
+
+    private void increaseCapacity() {
+        Object[] temp = new Object[values.length*2 + 1];
+        System.arraycopy(values, 0, temp, 0, pointer);
+        values = temp;
+    }
+}

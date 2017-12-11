8d5e0e7833a4d1424fd9f2a35c13b853bef18fa0
Awethon <gorodgorod63@gmail.com>
2017-12-11 18:13:59 +0300

    task13 impl

---
+++ src/main/java/com/epam/jf/polovinkin/homework/Task13.java
@@ -0,0 +1,14 @@
+package com.epam.jf.polovinkin.homework;
+
+public class Task13 extends com.epam.jf.common.homework.Task13 {
+    @Override
+    public int[][] createSquareMatrix(int dimension) {
+        int[][] matrix = new int[dimension][dimension];
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                matrix[i][j] = dimension * i + j + 1;
+            }
+        }
+        return matrix;
+    }
+}

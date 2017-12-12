715ef087fb0295cbf6b5cbd526141973b1a960f1
Awethon <gorodgorod63@gmail.com>
2017-12-12 03:12:23 +0300

    task15 all test passed

--- src/main/java/com/epam/jf/polovinkin/homework/Task15.java
+++ src/main/java/com/epam/jf/polovinkin/homework/Task15.java
@@ -5,29 +5,33 @@ public class Task15 extends com.epam.jf.common.homework.Task15 {
     public int[][] verticalCyclicShift(int[][] matrix, int numberShifts) {
         int[][] temp = new int[matrix.length][];
         int j = 0;
-        for (int i = (-numberShifts) % matrix.length; i < matrix.length; i++) {
-            temp[j] = matrix[i];
+        int start = (numberShifts % matrix.length + matrix.length) % matrix.length;
+
+        for (int i = start; i < matrix.length; i++) {
+            temp[i] = matrix[j];
             j++;
         }
-        for (int i = numberShifts % matrix.length; i < (-numberShifts) % matrix.length; i++) {
-            temp[j] = matrix[i];
+        for (int i = 0; i < start || j < matrix.length; i++) {
+            temp[i] = matrix[j];
             j++;
         }
-        return temp;
+        return temp.clone();
     }
 
     @Override
     public int[][] horizontalCyclicShift(int[][] matrix, int numberShifts) {
         int[][] temp = new int[matrix.length][matrix[0].length];
         int j = 0;
-        for (int i = (-numberShifts) % matrix.length; i < matrix.length; i++) {
-            for (int z = 0; z < matrix.length; z++) temp[z][j] = matrix[z][i];
+        int start = (numberShifts % matrix[0].length + matrix[0].length) % matrix[0].length;
+
+        for (int i = start; i < matrix[0].length; i++) {
+            for (int z = 0; z < matrix.length; z++) temp[z][i] = matrix[z][j];
             j++;
         }
-        for (int i = numberShifts % matrix.length; i < (-numberShifts) % matrix.length; i++) {
-            for (int z = 0; z < matrix.length; z++) temp[z][j] = matrix[z][i];
+        for (int i = 0; i < start || j < matrix[0].length; i++) {
+            for (int z = 0; z < matrix.length; z++) temp[z][i] = matrix[z][j];
             j++;
         }
-        return temp;
+        return temp.clone();
     }
 }

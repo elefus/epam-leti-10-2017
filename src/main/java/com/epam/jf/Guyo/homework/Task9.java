7d0e413f92a806330883aaaa3a1ea483fe504f92
ampiq <davidqa2011@yandex.ru>
2017-12-12 16:59:36 +0300

    Added task9 and test.

---
+++ src/main/java/com/epam/jf/Guyo/homework/Task9.java
@@ -0,0 +1,9 @@
+package com.epam.jf.Guyo.homework;
+
+public class Task9 extends com.epam.jf.common.homework.Task9 {
+
+    @Override
+    public boolean isPowerOfTwo(long value){
+        return (value != 0) && ((value & (value - 1)) == 0);
+    }
+}

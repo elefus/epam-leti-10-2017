1a7bdb5801a8c15aef96ff27e324f2abdf534c78
Ilya Ilyinykh <IlyasYOY@gmail.com>
2017-12-13 23:20:28 +0300

    Task20 done, test still needed

---
+++ src/main/java/com/epam/jf/ilyinykh/homework/Task20Done.java
@@ -0,0 +1,58 @@
+package com.epam.jf.ilyinykh.homework;
+
+import com.epam.jf.common.homework.Task20;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+
+public class Task20Done extends Task20 {
+
+    /**
+     * Формирует упорядоченный список строк из стихотворений указанного автора.
+     *
+     * @param poems  Анализируемое множество стихотворений.
+     * @param author Автор, стихотворения которого необходимо выбрать.
+     * @return Список, упорядоченных по длине строк, составляющих стихотворения автора.
+     */
+    @Override
+    public List<String> sortPoems(Set<IPoem> poems, String author) {
+        ArrayList<String> lines = new ArrayList<>();
+
+        for (IPoem poem : poems) {
+            if (author.equals(poem.getAuthor())) {
+                lines.addAll(poem.getLines());
+            }
+        }
+        lines.sort(Comparator.comparingInt(String::length));
+
+        return lines;
+    }
+}
+
+class Poem implements Task20.IPoem {
+    private List<String> strings;
+    private String author;
+
+    public Poem(List<String> strings, String author) {
+        this.strings = strings;
+        this.author = author;
+    }
+
+    /**
+     * @return Список строк, составляющих стихотворение.
+     */
+    @Override
+    public List<String> getLines() {
+        return null;
+    }
+
+    /**
+     * @return Автор стихотворения.
+     */
+    @Override
+    public String getAuthor() {
+        return null;
+    }
+}
---
+++ src/main/test/com/epam/jf/ilyinykh/homework/Task20DoneTest.java
@@ -0,0 +1,10 @@
+package com.epam.jf.ilyinykh.homework;
+
+import org.junit.jupiter.api.Test;
+
+class Task20DoneTest {
+
+    @Test
+    void sortPoems() {
+    }
+}

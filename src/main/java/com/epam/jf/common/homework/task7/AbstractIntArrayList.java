963c08ebd5011e37ab3292e00bd6553915c21a27
Guba Dmitriy <gudmian@gmail.com>
2017-12-16 01:35:21 +0300

    Made edits based on changes request

--- src/main/java/com/epam/jf/common/homework/task7/AbstractIntArrayList.java
+++ src/main/java/com/epam/jf/common/homework/task7/AbstractIntArrayList.java
@@ -2,50 +2,50 @@ package com.epam.jf.common.homework.task7;
 
 public abstract class AbstractIntArrayList {
 
-  protected int[] values;
+    private static final int DEFAULT_SIZE = 10;
 
-  public AbstractIntArrayList() {
-    this.values = new int[10];
-  }
+    protected int[] values;
 
-  public AbstractIntArrayList(int capacity) {
-    this.values = new int[capacity];
-  }
-
-  public AbstractIntArrayList(AbstractIntArrayList list) {
-    this.values = new int[(int) (list.values.length * 1.5)];
-    for (int i = 0; i < list.size(); i++) {
-      this.values[i] = list.values[i];
+    public AbstractIntArrayList() {
+        this.values = new int[DEFAULT_SIZE];
     }
-  }
 
-  public abstract boolean add(int value);
+    public AbstractIntArrayList(int capacity) {
+        this.values = new int[capacity];
+    }
 
-  public abstract boolean add(int value, int index);
+    public AbstractIntArrayList(AbstractIntArrayList list) {
+        values = new int[(int) (list.values.length)];
+        System.arraycopy(list.values, 0, values, 0, list.size());
+    }
 
-  public abstract int get(int index);
+    public abstract boolean add(int value);
 
-  public abstract boolean contains(int value);
+    public abstract boolean add(int value, int index);
 
-  public abstract boolean containsAll(AbstractIntArrayList list);
+    public abstract int get(int index);
 
-  public abstract int remove(int index);
+    public abstract boolean contains(int value);
 
-  public abstract int set(int value, int index);
+    public abstract boolean containsAll(AbstractIntArrayList list);
 
-  public abstract boolean addAll(AbstractIntArrayList list);
+    public abstract int remove(int index);
 
-  public abstract void clear();
+    public abstract int set(int value, int index);
 
-  public abstract int size();
+    public abstract boolean addAll(AbstractIntArrayList list);
 
-  public abstract boolean isEmpty();
+    public abstract void clear();
 
-  public abstract int indexOf(int value);
+    public abstract int size();
 
-  public abstract int lastIndexOf(int value);
+    public abstract boolean isEmpty();
 
-  public abstract AbstractIntArrayList subList(int fromInclusive, int toInclusive);
+    public abstract int indexOf(int value);
+
+    public abstract int lastIndexOf(int value);
+
+    public abstract AbstractIntArrayList subList(int fromInclusive, int toInclusive);
 
 
 }
--- src/main/java/com/epam/jf/guba/homework/task7/NoSuchElementException.java
+++
@@ -1,8 +0,0 @@
-package com.epam.jf.guba.homework.task7;
-
-public class NoSuchElementException extends RuntimeException {
-
-  public NoSuchElementException(String message) {
-    super(message);
-  }
-}

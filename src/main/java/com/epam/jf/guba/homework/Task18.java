abfdb50a6141f457d604b9dfd3000658e5dbb895
Guba Dmitriy <gudmian@gmail.com>
2017-12-16 15:19:30 +0300

    Task18 completed (Guba)

--- src/main/java/com/epam/jf/guba/homework/Task18.java
+++ src/main/java/com/epam/jf/guba/homework/Task18.java
@@ -11,12 +11,23 @@ public class Task18 extends com.epam.jf.common.homework.Task18 {
         }
         int secStart = 0;
         int secStop = 0;
+        int left =0;
+        int right = 0;
+        int maxLength = 0;
         for (int i = 0; i < source.size()-1; i++){
             int secLength = 0;
-            while (i < source.size()-1 &&  source.get(i).compareTo(source.get(i+1)) == 1){
+            secStart = i;
+            while (i < source.size()-1 && source.get(i).compareTo(source.get(i + 1)) < 0){
                 secLength++;
+                i++;
+            }
+            secStop = i;
+            if(secLength > maxLength){
+                left = secStart;
+                right = secStop;
+                maxLength = secLength;
             }
         }
-        return null;
+        return source.subList(left, right+1);
     }
 }

909353602cb1eb2f437edc777a2edaf850a80cd9
Guba Dmitriy <gudmian@gmail.com>
2017-12-31 13:21:01 +0300

    Task21 completed (Guba)

--- src/test/java/com/epam/jf/common/classwork/lesson13/PersonTest.java
+++ src/test/java/com/epam/jf/common/classwork/lesson13/PersonTest.java
@@ -6,22 +6,4 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
 
-    @Test
-    void testToString() {
-        Person person = Person.builder()
-                              .lastName("Ivanov")
-                              .firstName("Ivan")
-                              .patronymic("Ivanovich")
-                              .serial(0)
-                              .number(1)
-                              .build();
-
-        assertEquals("Person(hidden=100, firstName=Ivan, lastName=Ivanov, patronymic=Ivanovich, serial=0, number=1)", person.toString());
-    }
-
-    @Test
-    void testNameGetter() throws NoSuchMethodException {
-        Person person = new Person("Ivan", "Ivanov", "Ivanovich", 0, 1);
-        assertEquals("Ivan", person.getFirstName());
-    }
 }

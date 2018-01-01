54ff97e279fde45299db7e7c03f1a65c9b8ff6d1
Alex Burachenko <ss47305@gmail.com>
2018-01-01 23:28:08 +0300

    Task19 поправлен

--- src/main/java/com/epam/jf/burachenko/homework/Task19.java
+++ src/main/java/com/epam/jf/burachenko/homework/Task19.java
@@ -3,12 +3,10 @@ package com.epam.jf.burachenko.homework;
 public class Task19 extends com.epam.jf.common.homework.Task19 {
     @Override
     public int[][] rotateMatrixCounterclockwise(int[][] source) {
-        int n = source.length;
-        int m = source[0].length;
-        int[][] newMatrix = new int[m][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                newMatrix[m-j-1][i] = source[i][j];
+        int[][] newMatrix = new int[source[0].length][source.length];
+        for (int i = 0; i < source.length; i++) {
+            for (int j = 0; j < source[0].length; j++) {
+                newMatrix[source[0].length-j-1][i] = source[i][j];
             }
         }
         return newMatrix;

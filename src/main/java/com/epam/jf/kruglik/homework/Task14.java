68b797e79ba504b960c0756bc8b3fc54b597819a
kruglikalexey <kruglikalexey@ya.ru>
2018-01-02 22:45:25 +0300

    return matrix problem was fixed

--- src/main/java/com/epam/jf/kruglik/homework/Task14.java
+++ src/main/java/com/epam/jf/kruglik/homework/Task14.java
@@ -11,16 +11,21 @@ public class Task14 {
      * @return Новая матрица, содержащая результат применения операции.
      */
     public static int[][] sortMatrixByValuesInColumn(int[][] matrix, int indexColumn) {
-        for (int i = matrix.length - 1; i > 0; i--) {
+        int[][] resultMatrix = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; ++i){
+            System.arraycopy(matrix[i], 0, resultMatrix[i], 0, matrix[0].length);
+        }
+
+        for (int i = resultMatrix.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
-                if (matrix[j][indexColumn] > matrix[j + 1][indexColumn]) {
-                    int[] sch = matrix[j];
-                    matrix[j] = matrix[j + 1];
-                    matrix[j + 1] = sch;
+                if (resultMatrix[j][indexColumn] > resultMatrix[j + 1][indexColumn]) {
+                    int[] sch = resultMatrix[j];
+                    resultMatrix[j] = resultMatrix[j + 1];
+                    resultMatrix[j + 1] = sch;
                 }
             }
         }
-        return matrix;
+        return resultMatrix;
     }
 
 }
--- test/com/epam/jf/kruglik/homework/Task14Test.java
+++ test/com/epam/jf/kruglik/homework/Task14Test.java
@@ -8,11 +8,11 @@ class Task14Test {
     @Test
     void threeOrderMatrixSecondColumn() {
         int[][] matrix = {{1,2,6},
-                          {4,5,3},
-                          {7,8,9}};
+                {4,5,3},
+                {7,8,9}};
         int[][] resultMatrix = {{4,5,3},
-                                {1,2,6},
-                                {7,8,9}};
+                {1,2,6},
+                {7,8,9}};
 
         matrix = Task14.sortMatrixByValuesInColumn(matrix,2);
         assertArrayEquals(matrix,resultMatrix);
@@ -21,11 +21,11 @@ class Task14Test {
     @Test
     void threeOrderMatrixFirstColumn() {
         int[][] matrix = {{1,8,6},
-                          {4,5,3},
-                          {7,2,9}};
+                {4,5,3},
+                {7,2,9}};
         int[][] resultMatrix = {{7,2,9},
-                                {4,5,3},
-                                {1,8,6}};
+                {4,5,3},
+                {1,8,6}};
 
         matrix = Task14.sortMatrixByValuesInColumn(matrix,1);
         assertArrayEquals(matrix,resultMatrix);

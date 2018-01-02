96cfe96d5a81e4357117fe198209b5405d55b9ed
Alex Burachenko <ss47305@gmail.com>
2018-01-02 15:14:02 +0300

    Task22 сделан с тестами

--- src/main/java/com/epam/jf/burachenko/homework/Task22.java
+++ src/main/java/com/epam/jf/burachenko/homework/Task22.java
@@ -1,10 +1,12 @@
 package com.epam.jf.burachenko.homework;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.HashMap;
 
 public class Task22 extends com.epam.jf.common.homework.Task22 {
     @Override
-    public HashMap<Integer, Integer> addPolynomials(HashMap<Integer, Integer> first, HashMap<Integer, Integer> second) {
+    public HashMap<Integer, Integer> addPolynomials(@NotNull HashMap<Integer, Integer> first, @NotNull HashMap<Integer, Integer> second) {
         HashMap<Integer, Integer> result = new HashMap<>(first);
         for (Integer key : second.keySet()) {
             result.merge(key, second.get(key), (int1, int2) -> int1+int2);
--- src/test/java/com/epam/jf/burachenko/homework/Task22Test.java
+++ src/test/java/com/epam/jf/burachenko/homework/Task22Test.java
@@ -14,15 +14,36 @@ class Task22Test {
         first.put(0, 1);
         first.put(1, 2);
         first.put(2, 1);
-        System.out.println(first);
 
         HashMap<Integer, Integer> second = new HashMap<>();
         second.put(0, 5);
         second.put(3, 4);
         second.put(2, 4);
-        System.out.println(second);
 
-        HashMap<Integer, Integer> result = T.addPolynomials(first, second);
-        System.out.println(result);
+        HashMap<Integer, Integer> expected = new HashMap<>();
+        expected.put(0, 6);
+        expected.put(1, 2);
+        expected.put(2, 5);
+        expected.put(3, 4);
+
+        assertTrue(T.addPolynomials(first, second).equals(expected));
+        assertTrue(T.addPolynomials(second, first).equals(expected));
+
+
+        HashMap<Integer, Integer> first1 = new HashMap<>();
+        first1.put(0, 3);
+        first1.put(1, 5);
+
+        HashMap<Integer, Integer> second1 = new HashMap<>();
+        second1.put(0, 2);
+        second1.put(2, 4);
+
+        HashMap<Integer, Integer> expected1 = new HashMap<>();
+        expected1.put(0, 5);
+        expected1.put(1, 5);
+        expected1.put(2, 4);
+
+        assertTrue(T.addPolynomials(first1, second1).equals(expected1));
+        assertTrue(T.addPolynomials(second1, first1).equals(expected1));
     }
 }

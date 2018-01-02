ee6a2f746750b6855a01e2121ecdf47af349fed8
Alex Burachenko <ss47305@gmail.com>
2018-01-02 20:08:37 +0300

    intermediate commit

---
+++ src/main/java/com/epam/jf/burachenko/homework/task17/GenericLinkedList.java
@@ -0,0 +1,8 @@
+package com.epam.jf.burachenko.homework.task17;
+
+/**
+ * Реализация двунаправленного связного не-кольцевого списка.
+ * @param <E> Тип элементов, хранящихся в списке.
+ */
+public class GenericLinkedList<E> extends AbstractGenericList<E> {
+}

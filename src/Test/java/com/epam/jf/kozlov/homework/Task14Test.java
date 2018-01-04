81dd66d1972e6e2eb9b36b63f1f1821b390e9379
Denis Kozlov <ConstrayRUS@gmail.com>
2018-01-04 11:22:06 +0400

    Deleted triangular matrix tests

--- src/Test/java/com/epam/jf/kozlov/homework/Task14Test.java
+++ src/Test/java/com/epam/jf/kozlov/homework/Task14Test.java
@@ -7,13 +7,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class Task14Test {
 
     @Test
-    void nonRectangularMatrixTest() {
-        Task14 task = new Task14();
-        int[][] matrix = task.sortMatrixByValuesInColumn(new int[][]{{1, 2, 3, 4}, {4, 5, 6}, {1, 2}}, 1);
-        assertArrayEquals(new int[][]{{1, 2, 3, 4}, {1, 2}, {4, 5, 6}}, matrix);
-    }
-
-    @Test
     void oneRowMatrixTest() {
         Task14 task = new Task14();
         int[][] matrix = task.sortMatrixByValuesInColumn(new int[][]{{1, 3, 4}}, 0);
@@ -30,7 +23,7 @@ class Task14Test {
     @Test
     void arrayIndexOutOfBoundsTestMatrixTest() {
         Task14 task = new Task14();
-        assertThrows(ArrayIndexOutOfBoundsException.class,() -> task.sortMatrixByValuesInColumn(new int[][]{{1, 2, 3, 4}, {4, 5, 6}, {1, 2}}, 2));
+        assertThrows(ArrayIndexOutOfBoundsException.class,() -> task.sortMatrixByValuesInColumn(new int[][]{{3, 2, 1}, {6, 5, 4}, {9, 8, 7}}, 3));
     }
 
 }
--- src/main/java/com/epam/jf/kozlov/homework/Task14.java
+++ src/main/java/com/epam/jf/kozlov/homework/Task14.java
@@ -13,9 +13,8 @@ public class Task14 extends com.epam.jf.common.homework.Task14 {
      * @return Новая матрица, содержащая результат применения операции.
      */
     public int[][] sortMatrixByValuesInColumn(int[][] matrix, int indexColumn) {
-        int [][] newMatrix = new int[matrix.length][];
+        int [][] newMatrix = new int[matrix.length][matrix[0].length];
         for (int i = 0; i < matrix.length; i++) {
-            newMatrix[i] = new int[matrix[i].length];
             System.arraycopy(matrix[i], 0, newMatrix[i], 0, matrix[i].length);
         }
         Arrays.sort(newMatrix, Comparator.comparingInt(o -> o[indexColumn]));

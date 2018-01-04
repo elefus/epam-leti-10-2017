0a3406ac4bba99d43f77a3e84c3ef375f5dcd26c
Denis Kozlov <ConstrayRUS@gmail.com>
2018-01-04 14:17:18 +0400

    Fixed annotations

--- src/main/java/com/epam/jf/kozlov/homework/Task9.java
+++ src/main/java/com/epam/jf/kozlov/homework/Task9.java
@@ -7,6 +7,7 @@ public class Task9 extends com.epam.jf.common.homework.Task9 {
      * @param value анализируемое не отрицательное число.
      * @return true - число является степенью двойки, false в противном случае.
      */
+    @Override
     public boolean isPowerOfTwo(long value) {
         return (value > 0) && ((value & (value - 1)) == 0);
     }

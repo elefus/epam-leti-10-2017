b4491f545faaa967657c23c93b4bc23a01dc97e7
Denis Kozlov <ConstrayRUS@gmail.com>
2018-01-04 14:21:29 +0400

    Fixed annotations

--- src/main/java/com/epam/jf/kozlov/homework/Task14.java
+++ src/main/java/com/epam/jf/kozlov/homework/Task14.java
@@ -12,6 +12,7 @@ public class Task14 extends com.epam.jf.common.homework.Task14 {
      * @param indexColumn Индекс столбца, по значениям которого выполняется сортировка.
      * @return Новая матрица, содержащая результат применения операции.
      */
+    @Override
     public int[][] sortMatrixByValuesInColumn(int[][] matrix, int indexColumn) {
         int [][] newMatrix = new int[matrix.length][matrix[0].length];
         for (int i = 0; i < matrix.length; i++) {

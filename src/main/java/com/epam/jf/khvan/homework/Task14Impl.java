892891d08af1314528a68710ed4e4b6fa634027c
Dimitriy <kwanmitya@gmail.com>
2018-01-07 01:18:23 +0300

    Task14 completed

--- src/main/java/com/epam/jf/khvan/homework/Task14Impl.java
+++ src/main/java/com/epam/jf/khvan/homework/Task14Impl.java
@@ -2,7 +2,7 @@ package com.epam.jf.khvan.homework;
 
 import com.epam.jf.common.homework.Task14;
 
-public class Task14Implement extends Task14 {
+public class Task14Impl extends Task14 {
     public static void main(String[] args) {
         int[][] matrixExample = { {1,2,3,4,11},{4,3,0,4,2},{5,3,2,1,2},{9,6,0,8,2},{9,6,0,8,3}};
         for (int i = 0; i < matrixExample[0].length; i++) {
@@ -12,7 +12,7 @@ public class Task14Implement extends Task14 {
             System.out.println();
         }
         System.out.println();
-        int [][] sortedMatrix = new Task14Implement().sortMatrixByValuesInColumn(matrixExample,4);
+        int [][] sortedMatrix = new Task14Impl().sortMatrixByValuesInColumn(matrixExample,4);
         for (int i = 0; i < sortedMatrix[0].length; i++) {
             for (int j = 0; j < sortedMatrix[0].length; j++) {
                 System.out.print(sortedMatrix[i][j] + " ");
--- src/test/com/epam/jf/khvan/homework/Task14Test.java
+++ src/test/com/epam/jf/khvan/homework/Task14Test.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-public class Task14ImplementTest {
+public class Task14Test {
 
     @Test
     void sortByColumnTest1() {

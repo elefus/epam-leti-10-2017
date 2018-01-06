668f47ff616f5c67be2c1f2aefddc9578f1b0f9d
Awethon <gorodgorod63@gmail.com>
2018-01-06 20:19:41 +0300

    OH MY GOD THAT'S SO GOOOOOOOOOD

--- src/main/java/com/epam/jf/polovinkin/homework/Task20.java
+++ src/main/java/com/epam/jf/polovinkin/homework/Task20.java
@@ -7,12 +7,14 @@ import java.util.stream.Stream;
 public class Task20 extends com.epam.jf.common.homework.Task20 {
     @Override
     public List<String> sortPoems(Set<IPoem> poems, String author) {
-        poems.stream()
-             .filter(x -> Objects.equals(x.getAuthor(), author))
-             .map(IPoem::getLines)
-             .reduce((a, b) -> Stream.concat(a.stream(), b.stream()).collect(Collectors.toList()))
-             .orElse(Collections.emptyList())
-             .sort(Comparator.comparingInt(String::length));
-        return null;
+        return poems.stream()
+                    .filter(x -> Objects.equals(x.getAuthor(), author))
+                    .map(IPoem::getLines)
+                    .reduce((a, b) -> Stream.concat(a.stream(), b.stream())
+                                            .collect(Collectors.toList()))
+                    .orElse(Collections.emptyList())
+                    .stream()
+                    .sorted(Comparator.comparingInt(String::length))
+                    .collect(Collectors.toList());
     }
 }

5c87390c99b4bfeb19b6cdf73738eaf16d5b88ba
Awethon <gorodgorod63@gmail.com>
2018-01-06 23:47:45 +0300

    OH MY GOD THAT'S SO GOOOOOOOOOD AND ALSO TESTS ADDED

--- src/main/java/com/epam/jf/polovinkin/homework/Task20.java
+++ src/main/java/com/epam/jf/polovinkin/homework/Task20.java
@@ -14,7 +14,30 @@ public class Task20 extends com.epam.jf.common.homework.Task20 {
                                             .collect(Collectors.toList()))
                     .orElse(Collections.emptyList())
                     .stream()
-                    .sorted(Comparator.comparingInt(String::length))
+                    .sorted((a, b) -> b.length() - a.length())
                     .collect(Collectors.toList());
     }
+
+    static class Poem implements IPoem {
+
+        private List<String> poems;
+        private String author;
+
+        public Poem(List<String> poems, String author) {
+            this.poems = poems;
+            this.author = author;
+        }
+
+        @Override
+        public List<String> getLines() {
+            return new ArrayList<>(poems);
+        }
+
+        @Override
+        public String getAuthor() {
+            return author;
+        }
+    }
 }
+
+
---
+++ src/test/java/com/epam/jf/polovinkin/homework/Task20Test.java
@@ -0,0 +1,21 @@
+package com.epam.jf.polovinkin.homework;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task20Test {
+
+    @Test
+    void sortPoems() {
+        Set<Task20.IPoem> poems = new HashSet<>();
+        poems.add(new Task20.Poem(Arrays.asList("Sa1s", "sadasd", "asdas1d"), "author1"));
+        poems.add(new Task20.Poem(Arrays.asList("Sas", "sadasd", "asdasd"), "author2"));
+        poems.add(new Task20.Poem(Arrays.asList("qqqq", "qqqqqqq", "aqqqqqsdasd"), "author1"));
+
+        String[] actualStrings = {"aqqqqqsdasd", "qqqqqqq", "asdas1d", "sadasd", "qqqq", "Sa1s"};
+        assertArrayEquals(actualStrings, new Task20().sortPoems(poems, "author1").toArray());
+    }
+}

21f3cfb111f4b215b7d18653b28fb64125d27a35
Andrewisnew <zakomornii1998@gmail.com>
2018-01-11 03:28:12 +0300

    A new matrix was created on the basis of the source, instead of its modification

--- src/main/java/com/epam/jf/zakomornyy/homework/Task14.java
+++ src/main/java/com/epam/jf/zakomornyy/homework/Task14.java
@@ -4,18 +4,22 @@ package com.epam.jf.zakomornyy.homework;
 public class Task14 extends com.epam.jf.common.homework.Task14 {
     @Override
     public int[][] sortMatrixByValuesInColumn(int[][] matrix, int indexColumn) {
-        for (int min = 0; min < matrix.length - 1; min++) {
+        int[][] resultMatrix = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; ++i){
+            System.arraycopy(matrix[i], 0, resultMatrix[i], 0, matrix[0].length);
+        }
+        for (int min = 0; min < resultMatrix.length - 1; min++) {
             int least = min;
-            for (int j = min + 1; j < matrix.length; j++) {
-                if (matrix[j][indexColumn] < matrix[least][indexColumn]) {
+            for (int j = min + 1; j < resultMatrix.length; j++) {
+                if (resultMatrix[j][indexColumn] < resultMatrix[least][indexColumn]) {
                     least = j;
                 }
             }
-            int[] tmp = matrix[min];
-            matrix[min] = matrix[least];
-            matrix[least] = tmp;
+            int[] tmp = resultMatrix[min];
+            resultMatrix[min] = resultMatrix[least];
+            resultMatrix[least] = tmp;
         }
-        return matrix;
+        return resultMatrix;
     }
 
 

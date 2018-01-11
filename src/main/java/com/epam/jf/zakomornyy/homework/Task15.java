9534afe7375f79ccf7189516f5fa346abb97c841
Andrewisnew <zakomornii1998@gmail.com>
2018-01-11 03:38:54 +0300

    A new matrix was created on the basis of the source, instead of its modification

--- src/main/java/com/epam/jf/zakomornyy/homework/Task15.java
+++ src/main/java/com/epam/jf/zakomornyy/homework/Task15.java
@@ -13,18 +13,22 @@ public class Task15 extends com.epam.jf.common.homework.Task15 {
     }
 
     private int[][] cyclicShift(int[][] matrix, int numberShifts, boolean isVertShift) {
+        int[][] resultMatrix = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; ++i){
+            System.arraycopy(matrix[i], 0, resultMatrix[i], 0, matrix[0].length);
+        }
         boolean[] isChanged;
         int numOfChanged = 0;
         int[] temp1;
         if(isVertShift) {
-            isChanged = new boolean[matrix[0].length];
-            temp1 = new int[matrix.length];
+            isChanged = new boolean[resultMatrix[0].length];
+            temp1 = new int[resultMatrix.length];
             for(int i = 0; i < temp1.length; i++) {
-                temp1[i] = matrix[i][0];
+                temp1[i] = resultMatrix[i][0];
             }
         } else {
-            isChanged = new boolean[matrix.length];
-            temp1 = matrix[0];
+            isChanged = new boolean[resultMatrix.length];
+            temp1 = resultMatrix[0];
         }
         for (int i = 0; numOfChanged < isChanged.length;) {
             if(!isChanged[i]) {
@@ -36,16 +40,16 @@ public class Task15 extends com.epam.jf.common.homework.Task15 {
                 }
                 int[] temp2;
                 if(isVertShift) {
-                    temp2 = new int[matrix.length];
+                    temp2 = new int[resultMatrix.length];
                     for(int j = 0; j < temp1.length; j++) {
-                        temp2[j] = matrix[j][nextIndex];
+                        temp2[j] = resultMatrix[j][nextIndex];
                     }
                     for (int j = 0; j < temp2.length; j++){
-                        matrix[j][nextIndex] = temp1[j];
+                        resultMatrix[j][nextIndex] = temp1[j];
                     }
                 }else {
-                    temp2 = matrix[nextIndex];
-                    matrix[nextIndex] = temp1;
+                    temp2 = resultMatrix[nextIndex];
+                    resultMatrix[nextIndex] = temp1;
                 }
                 temp1 = temp2;
                 i = nextIndex;
@@ -56,13 +60,13 @@ public class Task15 extends com.epam.jf.common.homework.Task15 {
                 }
                 if(isVertShift) {
                     for(int j = 0; j < temp1.length; j++) {
-                        temp1[j] = matrix[j][i];
+                        temp1[j] = resultMatrix[j][i];
                     }
                 } else {
-                    temp1 = matrix[i];
+                    temp1 = resultMatrix[i];
                 }
             }
         }
-        return matrix;
+        return resultMatrix;
     }
 }

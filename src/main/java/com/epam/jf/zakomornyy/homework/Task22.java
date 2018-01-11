afb3a84f7a38101a8167c64890065a2311105ac1
Andrewisnew <zakomornii1998@gmail.com>
2018-01-11 03:22:24 +0300

    A new HashMap was created on the basis of the first, instead of its modification

--- src/main/java/com/epam/jf/zakomornyy/homework/Task22.java
+++ src/main/java/com/epam/jf/zakomornyy/homework/Task22.java
@@ -6,13 +6,14 @@ import java.util.Map;
 public class Task22 extends com.epam.jf.common.homework.Task22{
     @Override
     public HashMap<Integer, Integer> addPolynomials(HashMap<Integer, Integer> first, HashMap<Integer, Integer> second) {
+        HashMap<Integer, Integer> result = new HashMap<>(first);
         for (Map.Entry<Integer, Integer> entry : second.entrySet()) {
-            if(first.containsKey(entry.getKey())) {
-                first.put(entry.getKey(), entry.getValue() + first.get(entry.getKey()));
+            if(result.containsKey(entry.getKey())) {
+                result.put(entry.getKey(), entry.getValue() + result.get(entry.getKey()));
             } else {
-                first.put(entry.getKey(), entry.getValue());
+                result.put(entry.getKey(), entry.getValue());
             }
         }
-        return first;
+        return result;
     }
 }
